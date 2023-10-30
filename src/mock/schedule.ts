41ab40eab20b5a986f6858435273526b23fa4694
XPA <xpadev@gmail.com>
2023-10-30 21:25:28 +0900

    add: mockdata

---
+++ src/mock/schedule.ts
@@ -0,0 +1,93 @@
+import {TSchedule} from "../components/calendar/@types/schedule";
+import dayjs from "dayjs";
+
+export const MockSchedule:TSchedule[] = [
+  {
+    "name": "hogehoge",
+    "startTime": dayjs(new Date("2023-10-17T19:00:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-17T20:00:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": true,
+    "color": "#039be5",
+  },
+  {
+    "name": "Kubernetes Meetup Tokyo #61",
+    "startTime": dayjs(new Date("2023-10-19T19:00:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-19T21:05:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "はじめまして！若手エンジニアふんわりLT Night！ ",
+    "startTime": dayjs(new Date("2023-10-20T18:30:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-20T22:00:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "CTOA 勉強会",
+    "startTime": dayjs(new Date("2023-10-25T19:30:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-25T22:00:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "めぐろLT #8",
+    "startTime": dayjs(new Date("2023-10-26T19:30:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-26T21:30:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "magische mtg",
+    "startTime": dayjs(new Date("2023-10-30T21:00:00+09:00")),
+    "endTime": dayjs(new Date("2023-10-30T22:00:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "ZOZO Tech Meetup",
+    "startTime": dayjs(new Date("2023-11-06T19:00:00+09:00")),
+    "endTime": dayjs(new Date("2023-11-06T21:10:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  },
+  {
+    "name": "aaaaaaaaaaa",
+    "startTime": dayjs(new Date("2024-01-19T17:00:00+09:00")),
+    "endTime": dayjs(new Date("2024-01-19T18:00:00+09:00")),
+    "startDate": null,
+    "endDate": null,
+    "isAllDay": false,
+    "url": "",
+    "displayOnly": false,
+    "color": "#039be5"
+  }
+];
